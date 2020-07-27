dfc39fb356e8bbbcacf0432eaa4690caf9e859e5
Jiaxiao Zhou <duibao55328@gmail.com>
2020-07-27 12:10:21 -0700

    added firebase:

--- mlab/src/components/Firebase/firebase.js
+++ mlab/src/components/Firebase/firebase.js
@@ -1,8 +1,22 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
 const config = {
-  apiKey: YOUR_API_KEY,
-  authDomain: YOUR_AUTH_DOMAIN,
-  databaseURL: YOUR_DATABASE_URL,
-  projectId: YOUR_PROJECT_ID,
-  storageBucket: "",
-  messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyD1i4IAZ5zHQ0K8KBaYQ4cbCg_jXrdO77Q",
+  authDomain: "mlab-e47b1.firebaseapp.com",
+  databaseURL: "https://mlab-e47b1.firebaseio.com",
+  projectId: "mlab-e47b1",
+  storageBucket: "mlab-e47b1.appspot.com",
+  messagingSenderId: "237273132243",
+  appId: "1:237273132243:web:bcdc2ff5ec481f0a633826"
 };
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+}
+ 
+export default Firebase;
